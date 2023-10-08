2ca4767ef92bec3d9eb1a5eb38bd00960c42db59
mmyszak999 <71217112+mmyszak999@users.noreply.github.com>
2023-10-09 01:04:25 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2,
+    "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
